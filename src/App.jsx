55e01992adb290965d682cbf0837358cc82df196
Saeid Gholami <gholami_0101@yahoo.com>
2024-12-09 01:22:19 +0330

    create navbar using react

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import Footer from "./Footer/Footer";
-import Header from "./Header/Header";
-import Main from "./Main/Main";
+import "./App.css"
 
 function App() {
     return (
         <div>
-            <Header></Header>
-            <Main></Main>
-            <Footer></Footer>
+            <div className="navbar">
+                <div className="navbar__items">
+                    <ul className="navbar__items-ul">
+                        <li><a href="#" className="navbar__items-link">Home</a></li>
+                        <li><a href="#" className="navbar__items-link">Products</a></li>
+                        <li><a href="#" className="navbar__items-link">Services</a></li>
+                        <li><a href="#" className="navbar__items-link">Search</a></li>
+                        <li><a href="#" className="navbar__items-link">Log in</a></li>
+                        <li><a href="#" className="navbar__items-link">About us</a></li>
+                    </ul>
+                </div>
+                <div className="navbar__logo">
+                    <h1 className="navbar__logo-title">Saeid Gholami</h1>
+                    <img src="public/vite.svg" alt="navbar logo" />
+                </div>
+            </div>
         </div>
     )
 }
