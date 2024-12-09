4c1bb7c4417ee7cb9ce5b36381050e033fc36073
Saeid Gholami <gholami_0101@yahoo.com>
2024-12-09 17:10:34 +0330

    coding works html template and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css"
 import Header from "./Header/Header";
+import Works from "./Main/Works";
 import Navbar from "./Navbar/Navbar";
 
 function App() {
     return (
         <div>
             <Navbar />
-            <Header/>
+            <Header />
+            <Works />
         </div>
     )
 }
